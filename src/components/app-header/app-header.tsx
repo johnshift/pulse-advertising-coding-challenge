3d9f028891049f7a4208b6e9b91978de90b999de
johnshift <johnshift.dev@gmail.com>
2026-01-13 02:20:32 +0800

    feat: add home page

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -7,12 +7,7 @@ export const AppHeader = () => {
   return (
     <header className='sticky top-0 z-50 border-b border-neutral-900 bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60'>
       <div className='mx-auto flex h-20 max-w-6xl items-center justify-between px-6'>
-        <Link
-          href='https://www.pulse-advertising.com/'
-          className='h-12 w-32'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
+        <Link href='/' className='h-12 w-32'>
           <Image
             src='https://www.pulse-advertising.com/wp-content/themes/pulse/assets/images/pulse_logo_white.png'
             alt='Pulse Logo'
